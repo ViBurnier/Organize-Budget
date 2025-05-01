9ffaed1af6c7aaaae539217342ac0b3d7e650e55
Vitor Burnier <burnvitor2@gmail.com>
2025-05-01 11:17:36 -0300

    feature/ validacao dos campos

--- Organize-Budget/src/components/AddBudget.jsx
+++ Organize-Budget/src/components/AddBudget.jsx
@@ -1,22 +1,74 @@
+import { yupResolver } from "@hookform/resolvers/yup";
 import { ArrowBigRightDash } from "lucide-react";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
 
-function AddBudGet(){
+function AddBudGet() {
+  const schema = yup.object().shape({
+    title: yup.string().required("Título é obrigatório"),
+    date: yup.date().required("Data obrigatória"),
+    price: yup
+      .number()
+      .typeError("Preço deve ser um número")
+      .positive("Preço precisa ser positivo")
+      .required("Preço obrigatório"),
+    description: yup.string().max(100, "Máximo 100 caracteres"),
+  });
 
-    return(
-        <div className="w-full bg-gray-500 p-2 ">
-            <div className="space-y-5 columns-2">
-                <input placeholder="TITLE" className="bg-white rounded-sm p-2 w-full" type="text" name="titleB" id="nameB" />
-                <input placeholder="DATE" className="bg-white rounded-sm p-2 w-full" type="text" name="dateB" id="dateB" />
-                <input placeholder="PRICE" className="bg-white rounded-sm p-2 w-full" type="text" name="priceB" id="priceB" />
-                <input placeholder="DESCRIPTION" className="bg-white rounded-sm p-2 w-full" type="text" name="descriptionB" id="descriptionB" />
-            </div>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }} = useForm({
+    resolver: yupResolver(schema),
+  });
 
-            <button>
-                <ArrowBigRightDash />
-            </button>
-        </div>
-    )
+  return (
+    <div className="w-full bg-gray-500 p-2 ">
+      <div className="space-y-5 columns-2">
+        <form onSubmit={handleSubmit((data) => console.log(data))}>
+          <input
+            {...register("title")}
+            placeholder="TITLE"
+            id="title"
+            className="bg-white rounded-sm p-2 w-full"
+          />
 
+          <input
+            {...register("date")}
+            placeholder="DATE"
+            id="date"
+            className="bg-white rounded-sm p-2 w-full"
+          />
+          <input
+            {...register("price")}
+            placeholder="PRICE"
+            id="price"
+            className="bg-white rounded-sm p-2 w-full"
+          />
+          <input
+            {...register("description")}
+            placeholder="DESCRIPTION"
+            id="description"
+            className="bg-white rounded-sm p-2 w-full"
+          />
+
+          <button type="submit">
+            <ArrowBigRightDash />
+          </button>
+          
+        </form>
+
+        
+      </div>
+
+      <p>
+            {errors.title?.message}
+            {errors.date?.message}
+            {errors.price?.message}
+            {errors.description?.message}
+        </p>
+    </div>
+  );
 }
 
 export default AddBudGet;
