7400a1bf646e52476d44a5466e5cde6057a0552f
Vitor Burnier <burnvitor2@gmail.com>
2025-05-06 17:35:34 -0300

    router

--- Organize-Budget/src/App.jsx
+++ Organize-Budget/src/App.jsx
@@ -1,51 +1,39 @@
 import './App.css'
 import Config from './components/config'
-import AddBudGet from './components/AddBudget';  
-import { useEffect, useState } from 'react';
+import { Link } from "react-router";
+import Home from './pages/Home'
+import List from './pages/List'
+
 import { Trash2 } from "lucide-react";
-import { v4 } from "uuid";
+
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
-
-  const [list, setList] = useState(JSON.parse(localStorage.getItem("list")) || [])
-
-  useEffect(() => {
-    localStorage.setItem("list", JSON.stringify(list))
-  }, [list])
-  
-  function onAddBudget(title, date, price, description){
-    const newBudget  = {
-      id: v4(),
-      title,
-      date,
-      price,
-      description,
-      isCompleted: false,
-    }
-    console.log(newBudget)
-    setList([...list, newBudget])
-  }
-
   return (
    <div className='space-y-10'>
+
     <nav className='text-xl font-medium text-white h-19 flex items-center justify-between'>
       
       <div className='flex w-25 justify-around'>
+        <Link to="/" className='flex'>
         <img src={Config.logo} alt="" className='h-10 w-10' />
         <p>{Config.title}</p>
+        </Link>
       </div>
 
       <div className='flex justify-around w-60'>
-        <button>Lista de Despesas</button>
+      <a href=""></a>
+        <Link to="/List">List</Link>
+
         <button><Trash2/></button>
       </div>
     </nav>
 
-    <AddBudGet
-      onAddBudget={onAddBudget}
-    />
+    <Routes>
+      <Route path='/' element={<Home />} />
 
-    
+      <Route path='/List' element={<List />} />
+    </Routes>
 
    </div>
   )
---
+++ Organize-Budget/src/pages/List.jsx
@@ -0,0 +1,13 @@
+function List(){
+    return(
+        <div className="flex justify-center">
+            <div className=" h-full w-[100rem] bg-amber-950 p-5">
+
+            
+
+            </div>
+        </div>
+    )
+}
+
+export default List;
