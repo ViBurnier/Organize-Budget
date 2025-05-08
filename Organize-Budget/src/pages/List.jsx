2c642424277593c51fbc58487e97652c24f25266
Vitor Burnier <burnvitor2@gmail.com>
2025-05-08 14:10:50 -0300

    fix/list map

--- Organize-Budget/src/pages/List.jsx
+++ Organize-Budget/src/pages/List.jsx
@@ -4,12 +4,12 @@ function List() {
   return (
     <div className="flex justify-center">
       <div className=" h-full w-[100rem] bg-amber-950 p-5">
-        {list.map((value, index) => (
-          <div key={index} className=" bg-amber-50 p-5 my-3">
-            <p>title: {value.title}</p>
-            <p>description: {value.description}</p>
-            <p>price: {value.price}</p>
-            <p>date: {value.date}</p>
+        {list.map(list => (
+          <div  className=" bg-amber-50 p-5 my-3">
+            <p>title: {list.title}</p>
+            <p>description: {list.description}</p>
+            <p>price: {list.price}</p>
+            <p>date: {list.date}</p>
           </div>
         ))}
       </div>
