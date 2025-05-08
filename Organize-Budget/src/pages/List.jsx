33ccccb4b2d8571a706b2bacbfc67dcb204f4180
Vitor Burnier <burnvitor2@gmail.com>
2025-05-08 13:49:45 -0300

    fix/list view

--- Organize-Budget/src/pages/List.jsx
+++ Organize-Budget/src/pages/List.jsx
@@ -1,44 +1,20 @@
-function List(){
+function List() {
+  const list = JSON.parse(localStorage.getItem("list"));
 
-    const list = JSON.parse( localStorage.getItem('list') )
-
-    //vai percorrer todo arr(lista de objeto
-
-    function construirLista(){
-        const arr = []
-        
-        for(let i = 0; i < list.length; i++){
-            const title =  list[i].title
-            const description = list[i].description
-            const date = list[i].date
-            const price = list[i].price
-
-            arr.push(
-                <div className=" bg-amber-50 p-5 my-3">
-                    <p>title: {title}</p>
-                    <p>description: {description}</p>        
-                    <p>price: {price}</p> 
-                    <p>date: {date}</p>
-                </div>
-            )  
-        }
-
-        return arr
-    }
-
-
-    return(
-
-        <div className="flex justify-center">
-            <div className=" h-full w-[100rem] bg-amber-950 p-5">
-
-                
-
-                {construirLista()}
-                
-            </div>
-        </div>
-    )
+  return (
+    <div className="flex justify-center">
+      <div className=" h-full w-[100rem] bg-amber-950 p-5">
+        {list.map((value, index) => (
+          <div key={index} className=" bg-amber-50 p-5 my-3">
+            <p>title: {value.title}</p>
+            <p>description: {value.description}</p>
+            <p>price: {value.price}</p>
+            <p>date: {value.date}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default List;
