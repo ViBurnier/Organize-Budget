65a2ce8c9f731623af74cf4cfa035aa2ce5e3e28
Vitor Burnier <burnvitor2@gmail.com>
2025-05-10 16:39:46 -0300

    feature/ delete a despesa da list

--- Organize-Budget/src/pages/List.jsx
+++ Organize-Budget/src/pages/List.jsx
@@ -1,7 +1,18 @@
+import { Trash2 } from "lucide-react";
+import { useState } from "react";
+
 function List() {
-  const list = JSON.parse(localStorage.getItem("list"));
+  
+  const [list, setList] = useState(JSON.parse(localStorage.getItem("list")) || [])
+
+  function onDeletBudget(id){
+    const a = list.filter(z => id != z.id)
+    localStorage.setItem("list", JSON.stringify(a))
+    setList(a)
+  }
 
   return (
+
     <div className="flex justify-center">
       <div className=" h-full w-[100rem] bg-amber-950 p-5">
         {list.map(list => (
@@ -10,10 +21,15 @@ function List() {
             <p>description: {list.description}</p>
             <p>price: {list.price}</p>
             <p>date: {list.date}</p>
+
+            <button onClick={() => { onDeletBudget(list.id)}}>
+              <Trash2 />
+            </button>
           </div>
         ))}
       </div>
     </div>
+
   );
 }
 
